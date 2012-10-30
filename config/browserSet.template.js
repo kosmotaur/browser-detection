ea03524ed571c97e1888884d8f07d405eb7269be
kosmotaur <marek.stasikowski@gmail.com>
2012-10-30 17:03:02 +0100

    default browser set update
    for current major browsers, their current major version and 2 past
    versions

--- config/browserSet.template.js
+++ config/browserSet.template.js
@@ -12,8 +12,9 @@
 /*global BT*/
 
 BT.targetBrowserSet = { // the set you are trying to determine if the browser is contained in — copy this file to browserSet.js and modify to your needs
-    "ie"      : ["8", "9", "10"],
-    "firefox" : ["15", "16", "17"],
+    "ie"      : ["7", "8", "9"],
+    "firefox" : ["14", "15", "16"],
     "chrome"  : ["20", "21", "22"],
-    "safari"  : ["5", "5.1", "6"]
+    "safari"  : ["4.0", "5.1", "6"],
+    "opera"   : ["10.6", "11.6", "12.5"]
 };
