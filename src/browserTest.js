2aeea851ef85446a825af8030c60cf6e38d73584
kosmotaur <marek.stasikowski@gmail.com>
2012-10-30 01:08:07 +0100

    client side

--- src/browserTest.js
+++ src/browserTest.js
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2012 Marek Stasikowski <marek.stasikowski@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 /*jslint sloppy: true, vars: true, white: true, plusplus: true, newcap: true, nomen: true */
 /*global Modernizr, _, BT, navigator*/
 
@@ -36,13 +45,6 @@ BT._compare = function(setA, setB) {
     });
 
     return diff;
-};
-
-BT.targetBrowserSet = { // the set you are trying to determine  — you can change this to your set of interest in your script by extending/overwriting this object
-    "ie"      : ["8", "9"],
-    "firefox" : ["14", "15", "16"],
-    "chrome"  : ["20", "21"],
-    "safari"  : ["5", "5.1", "6"]
 };
 
 BT.detectByFeatures = function() {
